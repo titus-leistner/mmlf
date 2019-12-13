85352bffb176466c2f21a55de3e68724c69fdd5a
Titus Leistner <mail@titus-leistner.de>
2019-12-13 18:55:16 +0100

    Add L1 loss

--- mmlf/validate/sparsify.py
+++ mmlf/validate/sparsify.py
@@ -10,7 +10,7 @@ from ..utils import pfm
 
 def masked_mse(input, target, mask):
     """
-    Computed MSE loss only for pixels which are True in mask
+    Comput MSE loss only for pixels which are True in mask
 
     :param input: the prediction
     :type input: numpy.ndarray
@@ -24,7 +24,26 @@ def masked_mse(input, target, mask):
     diff = (input - target) ** 2.0
     count = np.sum(mask)
 
-    return np.sum(diff * mask.astype(np.float)) / count * 100.0
+    return np.sum(diff * mask.astype(np.float)) / count
+
+
+def masked_l1(input, target, mask):
+    """
+    Comput L1 loss only for pixels which are True in mask
+
+    :param input: the prediction
+    :type input: numpy.ndarray
+
+    :param target: the ground truth
+    :type target: numpy.ndarray
+
+    :param mask: the mask
+    :type mask: numpy.ndarray(dtype=numpy.bool)
+    """
+    diff = np.abs(input - target)
+    count = np.sum(mask)
+
+    return np.sum(diff * mask.astype(np.float)) / count
 
 
 def auc(curve, step):
@@ -47,8 +66,14 @@ def auc(curve, step):
 @click.command()
 @click.argument('output_dir', type=click.Path(exists=True))
 @click.option('--step', default=0.01, help='Step size for sparsification.')
-def main(output_dir, step):
-    loss_fn = masked_mse
+@click.option('--mse/--l1', default=True, help='Use MSE or L1 loss?')
+def main(output_dir, step, mse):
+    # set loss function
+    if mse:
+        loss_fn = masked_mse
+    else:
+        loss_fn = masked_l1
+
     scenes = [f.path for f in os.scandir(
         os.path.join(output_dir, 'scenes')) if f.is_dir()]
 
@@ -91,22 +116,28 @@ def main(output_dir, step):
     # fill in remaining and normalize
     loss[1:3] /= np.max(loss[1:3])
 
+    # reverse order
     loss = loss[:, ::-1]
+
+    # delete last element (1.0 makes no sense)
+    loss = np.delete(loss, -1, axis=1)
+
+    sparse_error = loss[2] - loss[1]
 
     with open(os.path.join(output_dir, 'sparsify.csv'), 'w') as f:
         # print and save result
-        header = 'frac,    oracle,    uncert'
+        header = 'frac,     oracle,     uncert, sparse_err'
         print(header)
         print(header, file=f)
         for i in range(loss.shape[1]):
-            line = f'{loss[0, i]:.2f}, {loss[1, i]:.8f}, {loss[2, i]:.8f}'
+            line = f'{loss[0, i]:.2f}, {loss[1, i]:.8f}, {loss[2, i]:.8f}, ' \
+                   f'{sparse_error[i]:.8f}'
             print(line)
             print(line, file=f)
 
-    print('----------------------------')
+    print('----------------------------------------')
     # compute area under the curve
-    curve = loss[2] - loss[1]
-    print('AUC: ', auc(curve, step))
+    print('AUC: ', auc(sparse_error, step))
 
 
 if __name__ == '__main__':
