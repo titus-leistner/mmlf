c71bd1d356997dfd19e9e99fd43648a489a4edf6
Titus Leistner <mail@titus-leistner.de>
2021-07-28 04:50:21 +0200

    Implement likelihood validation

--- mmlf/validate/cli.py
+++ mmlf/validate/cli.py
@@ -7,10 +7,35 @@ from ..model.feed_forward import FeedForward
 from ..model.ensamble import Ensamble
 # from ..model.invertible import ZixelWrapper
 from ..model import loss
+from ..utils.dl import mpi_to_weights
 
 import numpy as np
 import torch
 import click
+
+
+def likelihood_laplace(mpi, mean, logvar):
+    disp = mpi[:, :, 4]
+    alpha = mpi[:, :, 3]
+    mean = np.expand_dims(mean, 1)
+    var = np.exp(np.expand_dims(logvar, 1))
+
+    prob = np.exp(-(np.abs(mean - disp)) / var) / var / 2.0
+    lh = np.sum(alpha * prob, axis=1)
+
+    return np.mean(lh)
+
+
+def likelihood_lmm(mpi, means, logvars):
+    count = means.shape[0]
+    mean = means.reshape((-1, means.shape[2], means.shape[3]))
+    logvar = logvars.reshape((-1, logvars.shape[2], logvars.shape[3]))
+
+    return likelihood_laplace(mpi, mean, logvar) / float(count)
+
+
+def likelihood_discrete(weights, posterior):
+    return np.mean(weights * posterior)
 
 
 @click.command()
@@ -33,15 +58,16 @@ import click
 def main(output_dir, dataset, model_invertible, model_discrete,
          val_loss_margin, val_ensamble, val_disp_step, val_disp_min,
          val_disp_max):
-    valset = hci4d.HCI4D(dataset)
-    valloader = torch.utils.data.DataLoader(valset,
-                                            batch_size=1,
-                                            shuffle=False, num_workers=1)
 
     # load hyper parameters
     state = torch.load(os.path.join(output_dir, 'checkpoint.pt'))
     kwargs = state['hyper_parameters']
     kwargs.update({'model_discrete': model_discrete, 'val_disp_min': val_disp_min, 'val_disp_max': val_disp_max})
+
+    valset = hci4d.HCI4D(dataset, transform=hci4d.Shift(kwargs['train_shift']))
+    valloader = torch.utils.data.DataLoader(valset,
+                                            batch_size=1,
+                                            shuffle=False, num_workers=1)
 
     # if model_invertible:
     #     model = ZixelWrapper(**kwargs).cuda()
@@ -73,6 +99,7 @@ def main(output_dir, dataset, model_invertible, model_discrete,
 
         mse_avg = 0.0
         bad_pix_avg = 0.0
+        lh_avg = 0.0
         for i, data in enumerate(valloader):
             if i == len(valset.scenes):
                 break
@@ -98,23 +125,26 @@ def main(output_dir, dataset, model_invertible, model_discrete,
             bad_pix_avg += bad_pix
 
             # save results
-            mean = output['mean'].cpu().numpy()
+            if kwargs['model_discrete']:
+                weights = mpi_to_weights(mpi, model.disp_min, model.disp_max, model.steps).cpu().numpy()
+            mpi = mpi.cpu().numpy()
+            mean = output['mean'].cpu().numpy() + kwargs['train_shift']
 
             logvar = output.get('logvar', None)
             if logvar is not None:
                 logvar = logvar.cpu().numpy()
 
-            # GMM parameters
+            # LMM parameters
             means = output.get('means', None)
             logvars = output.get('logvars', None)
 
-            gmm = None
+            lmm = None
             if means is not None and logvars is not None:
                 means = means.cpu().numpy()
                 logvars = np.exp(logvars.cpu().numpy())
-                gmm = np.stack([means, logvars], 0)
+                lmm = np.stack([means, logvars], 0)
 
-            nll = output.get('nll', None)
+            nll = output.get('scores', None)
             if nll is not None:
                 nll = nll.cpu().numpy()
 
@@ -123,12 +153,28 @@ def main(output_dir, dataset, model_invertible, model_discrete,
                 posterior = posterior.cpu().numpy()
             runtime = time.time() - t_start
             valset.save_batch(output_dir, index.numpy(), mean,
-                              logvar, runtime, gmm, nll, posterior)
+                              logvar, runtime, lmm, nll, posterior)
+
+            if kwargs['val_ensamble']:
+                print('Evaluate likelihood for Laplacian Mixture Model...')
+                lh = likelihood_lmm(mpi, means, logvars)
+            elif kwargs['model_discrete']:
+                print('Evaluate likelihood for discrete posterior...')
+                lh = likelihood_discrete(weights, posterior)
+            elif kwargs['model_uncert']:
+                print('Evaluate likelihood for Laplacian distribution...')
+                lh = likelihood_laplace(mpi, mean, logvar)
+            else:
+                print('Evaluate likelihood for Laplacian distribution with b = 1 ...')
+                lh = likelihood_laplace(mpi, mean, np.ones_like(mean))
+
+            lh_avg += lh
 
         mse_avg /= (i + 1)
         bad_pix_avg /= (i + 1)
+        lh_avg /= (i + 1)
 
-    print(f'MSE: {mse_avg:.8f}, BadPix007: {bad_pix_avg:.8f}')
+    print(f'MSE: {mse_avg:.8f}, BadPix007: {bad_pix_avg:.8f}, Likelihood: {lh_avg:8f}')
 
 
 if __name__ == '__main__':
